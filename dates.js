6b4783174790446d7c8283bd7b233ab357cccbb1
nosoyivan <plascencia925@gmail.com>
2023-03-09 10:38:50 -0800

    Create dates.js

---
+++ dates.js
@@ -0,0 +1,23 @@
+// YYYY, MM(-1), DD 
+// 2001, 10(-1), 11 
+/// (9/11/2001) 
+
+export const events = [
+    {
+        title: "Project 1",
+        allDay: true,
+        start: new Date(2023, 2, 1),
+        end: new Date(2023, 2, 11),
+        comment: "info for Project #",
+    },
+    {
+        title: "Project 2",
+        start: new Date(2023, 2, 3),
+        comment: "info for Project #"
+    },
+    {
+        title: "Project 3",
+        start: new Date(2023, 2, 10),
+        comment: "info for Project #",
+    },
+];
